95804ddd5de67d2d017aab82fa56af274af01183
Gerd <gerd@gerd.at>
2018-05-21 18:31:30 +0200

    Fixed GPIO selection mode and added timing

--- control.py
+++ control.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
+import time
+GPIO.setmode(GPIO.BCM)
 
 RELAIS_UP_GPIO = 20
 RELAIS_DOWN_GPIO = 21
@@ -7,7 +8,10 @@ RELAIS_DOWN_GPIO = 21
 GPIO.setup(RELAIS_UP_GPIO, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(RELAIS_DOWN_GPIO, GPIO.OUT, initial=GPIO.LOW)
 
+time.sleep(5)
+
 GPIO.output(RELAIS_UP_GPIO, GPIO.HIGH)
+time.sleep(1)
 GPIO.output(RELAIS_UP_GPIO, GPIO.LOW)
 
 GPIO.cleanup()
